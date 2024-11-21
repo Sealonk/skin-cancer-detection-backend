47bba3d97462f6ca2d43b065031d7fc4f411ae17
Sealonk <aloysiusandrenhm@gmail.com>
2024-11-21 14:45:56 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,50 +1,33 @@
 const predictClassification = require('../services/inferenceService');
-const storeData = require('../services/storeData');
 const crypto = require('crypto');
-
+const storeData = require('../services/storeData');
+ 
 async function postPredictHandler(request, h) {
-    const { image } = request.payload;
-    const { model } = request.server.app;
+  const { image } = request.payload;
+  const { model } = request.server.app;
+ 
+  const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+ 
+  const data = {
+    "id": id,
+    "result": label,
+    "explanation": explanation,
+    "suggestion": suggestion,
+    "confidenceScore": confidenceScore,
+    "createdAt": createdAt
+  }
 
-    try {
-        // Memanggil fungsi predictClassification untuk melakukan prediksi
-        const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
-        const id = crypto.randomUUID();
-        const createdAt = new Date().toISOString();
-
-        // Data hasil prediksi
-        const data = {
-            id: id,
-            result: label,
-            explanation: explanation,
-            suggestion: suggestion,
-            confidenceScore: confidenceScore,
-            createdAt: createdAt
-        };
-
-        // Menyimpan data ke Firestore menggunakan storeData
-        await storeData(id, data);
-
-        // Mengirimkan respons sukses
-        const response = h.response({
-            status: 'success',
-            message: confidenceScore > 99 
-                ? 'Model is predicted successfully.' 
-                : 'Model is predicted successfully but under threshold. Please use the correct picture',
-            data
-        });
-        response.code(201);
-        return response;
-
-    } catch (error) {
-        // Menangani error dan mengirimkan respons gagal
-        const response = h.response({
-            status: 'fail',
-            message: error.message,
-        });
-        response.code(500);
-        return response;
-    }
+  await storeData(id, data);
+  
+  const response = h.response({
+    status: 'success',
+    message: confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
+    data
+  })
+  response.code(201);
+  return response;
 }
-
+ 
 module.exports = postPredictHandler;
